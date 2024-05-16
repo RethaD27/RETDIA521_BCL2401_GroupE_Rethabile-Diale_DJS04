add5dfff72ef39b5dda8e5ee58458f2932bf18bb
Rethabile Diale <rethadiale27@gmail.com>
2024-05-16 15:02:43 +0200

    Added function to create and append book previews to the DOM

--- scripts.js
+++ scripts.js
@@ -10,22 +10,14 @@ const getElement = (selector) => document.querySelector(selector);
 
 // Function to create and append book previews to the DOM
 const createBookPreviews = (books, container) => {
-  const fragment = document.createDocumentFragment();
   books.forEach(({ author, id, image, title }) => {
-    const element = document.createElement("button");
-    element.classList = "preview";
+    const element = document.createElement("book-preview");
+    element.setAttribute("image", image);
+    element.setAttribute("title", title);
+    element.setAttribute("author", authors[author]);
     element.setAttribute("data-preview", id);
-    element.innerHTML = `
-            <img class="preview__image" src="${image}" />
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `;
-
-    fragment.appendChild(element);
+    container.appendChild(element);
   });
-  container.appendChild(fragment);
 };
 
 // Initial rendering of book previews
